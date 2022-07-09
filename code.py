f56d2469f40fda818aaaec220bce8ce4a51f67f1
poojithapilli <74807102+poojithapilli@users.noreply.github.com>
2022-07-09 18:30:42 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,26 +1,17 @@
 import pandas as pd
 import re
-import spacy
-df=pd.read_csv("reviewslr.tsv")
+df=pd.read_csv("reviewslr.csv") #Load the data into python environment
 df.head()
 
+
+#Remove the punctuations which is noisy data.
 df['new_reviews'] = df['REVIEW_TEXT'].apply(lambda x: " ".join(x.lower() for x in x.split()))
 df['new_reviews'] = df['REVIEW_TEXT'].str.replace('[^\w\s]','')
 df['new_reviews'].head()
 
-from nltk.corpus import stopwords
-import nltk
-nltk.download('stopwords')
-stop = stopwords.words('english')
-df['new_reviews'] = df['new_reviews'].apply(lambda x: " ".join(x for x in x.split() if x not in stop))
-df.head()
 
-nlp = spacy.load('en', disable=['parser', 'ner'])
-def space(comment):
-    doc = nlp(comment)
-    return " ".join([token.lemma_ for token in doc])
-df['new_reviews']= df['new_reviews'].apply(space)
-df.head()
+
+#Removing unnecessary columns and extracting required columns.
 
 from sklearn.preprocessing import LabelEncoder
 df['labels'] = df['LABEL'].replace(['__label1__','__label2__'],['0','1'])
@@ -38,6 +29,7 @@ def Encoder(df):
           return df
 s=Encoder(X)
 
+#As the reviews are textual we use count vectorization inorder to convert to numerical data.
 from sklearn.feature_extraction.text import CountVectorizer
 vectorizer = CountVectorizer(token_pattern=r"(?u)\b\w+\b",analyzer='word',max_features=1400, min_df=1,stop_words=None)
 X_vectorizer = vectorizer.fit_transform(df['new_reviews'])
@@ -45,6 +37,7 @@ Y_vectorizer = vectorizer.transform(df['labels'])
 count_vect_df = pd.DataFrame(X_vectorizer.todense(), columns=vectorizer.get_feature_names())
 k=pd.concat([s,count_vect_df],axis=1)
 
+#Dividing the data into train and test in 80:20 ratio.
 from sklearn.model_selection import train_test_split
 from scipy.sparse import hstack
 from sklearn import preprocessing
@@ -54,11 +47,13 @@ df1=df[['new_reviews','VERIFIED_PURCHASE','RATING']]
 x_train,x_test,y_train,y_test=train_test_split(k,df['labels'],test_size=0.2, random_state=7)
 vectorizer = CountVectorizer(token_pattern=r"(?u)\b\w+\b",analyzer='word', min_df=1,stop_words=None)
 
+#Scaling the values from 0 to 1.
 scaler = MinMaxScaler()
 model=scaler.fit(x_train)
 scaled_data=model.transform(x_train)
 scaled_data_test=model.transform(x_test)
 
+#Training the Logistic Regression model and checking for accuracy and also tuning hyper parameters.
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import classification_report
 from sklearn.metrics import accuracy_score
@@ -70,29 +65,8 @@ report=classification_report(y_test,pred,output_dict=True)
 report_result = pd.DataFrame(report).transpose()
 report_result
 
-from sklearn.linear_model import PassiveAggressiveClassifier
-pac=PassiveAggressiveClassifier(max_iter=50)
-pac.fit(scaled_data,y_train)
-y_pred=pac.predict(scaled_data_test)
-score=accuracy_score(y_test,y_pred)
-print(f'Accuracy: {round(score*100,2)}%')
-report=classification_report(y_test,y_pred,output_dict=True)
-report_result = pd.DataFrame(report).transpose()
 
-from sklearn.ensemble import RandomForestClassifier
-lf = RandomForestClassifier(max_depth=2, random_state=0).fit(scaled_data, y_train)
-pre=lf.predict(scaled_data_test)
-score=accuracy_score(y_test,pre)
-print(f'Accuracy: {round(score*100,2)}%')
-print(classification_report(y_test,pre))
-
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB().fit(scaled_data,y_train)
-predicted=clf.predict(scaled_data_test)
-score=accuracy_score(y_test,predicted)
-print(f'Accuracy: {round(score*100,2)}%')
-print(classification_report(y_test,predicted))
-
+#Training the SVC model and checking for accuracy and also tuning hyper parameters.
 from sklearn.svm import SVC
 clf = SVC(C=10,gamma=0.0001,kernel='rbf').fit(scaled_data,y_train)
 predct=clf.predict(scaled_data_test)
